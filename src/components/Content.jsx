269e3873961694fcddf1ba1d0b3b514237efa395
BHAVYAPODDAR <poddarbhavya3@gmail.com>
2024-07-06 17:43:53 +0530

    Changes

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Content = ({ children }) => {
   return (
     <div className="w-screen">
-      <header className="flex items-center text-4xl justify-center mt-4 gap-4 h-[5vh]">
+      <header className="flex items-center text-3xl justify-center mt-4 gap-4 h-[5vh]">
         <h1 className="font-bold font-serif">Managely</h1>
       </header>
       {children}
